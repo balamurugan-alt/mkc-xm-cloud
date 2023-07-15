9c0970c8bead4247512c6aaa0f9332dd904b6829
balamurugan-alt <139472761+balamurugan-alt@users.noreply.github.com>
2023-07-15 22:25:04 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import { I18nProvider } from 'next-localization';
 import { SitecorePageProps } from 'lib/page-props';
-import Image from 'next/image';
+
 
 import 'assets/main.scss';
 
@@ -10,10 +10,6 @@ function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element
 
   return (
     <>
-      <Image
-        src="https://www.mccormick.com/-/media/project/oneweb/mccormick-us/frenchs/recipes/g/frenchs-green-bean-casserole-recipe.jpg"
-        alt="banner"
-      />
       <h1>MKC - XM Cloud</h1>
       <p>Starter Kit</p>
       <I18nProvider lngDict={dictionary} locale={pageProps.locale}>
